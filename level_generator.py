fd02ffc4d5be65dc158f4a012407eb7d71313f3f
HardreaM <46752979+HardreaM@users.noreply.github.com>
2022-02-01 20:18:44 +0500

    improved level generator

--- level_generator.py
+++ level_generator.py
@@ -1,26 +1,41 @@
 import os
 from PIL import Image
+from sys import argv
 
-path = os.path.abspath(os.getcwd())
+def main():
 
-img = Image.open(path + "/levels/test_level.png", "r")
-rgb_img = img.convert("RGB")
-pixels = list(img.getdata())
+    image_name, level_path = argv[1::]
 
-f = open(path + "/levels/test_level.txt", "w")
+    path = os.path.abspath(os.getcwd())
 
-c = 0
+    img = Image.open(path + "/levels/" + image_name, "r")
+    rgb_img = img.convert("RGB")
+    pixels = list(img.getdata())
 
-for i in range(0, 50):
-    for j in range(0, 75):
-        r, g, b = rgb_img.getpixel((j, i))
-        if r == 0 and g == 0 and b == 0:    #RGB(0, 0, 0) equals void
-            f.write("-")
-        elif r == 255 and g == 0 and b == 0:    #RGB(255, 0, 0) equals brick wall
-            f.write("*")
-        elif r == 0 and g == 255 and b == 0:    #RGB(0, 255, 0) equals grass
-            f.write("%")
-        elif r == 255 and g == 255 and b == 255: #RGB(255, 255, 255) equals steel wall
-            f.write("#") 
+    f = open(path + "/levels/" + level_path + ".txt", "w")
 
-    if i != 49: f.write("\n")
+    c = 0
+
+    for i in range(0, 50):
+        for j in range(0, 75):
+            r, g, b = rgb_img.getpixel((j, i))
+            if r == 0 and g == 0 and b == 0:    #RGB(0, 0, 0) equals void
+                f.write("-")
+            elif r == 255 and g == 0 and b == 0:    #RGB(255, 0, 0) equals brick wall
+                f.write("*")
+            elif r == 0 and g == 255 and b == 0:    #RGB(0, 255, 0) equals grass
+                f.write("%")
+            elif r == 255 and g == 255 and b == 255: #RGB(255, 255, 255) equals steel wall
+                f.write("#")
+            elif r == 50 and g == 50 and b == 50:
+                f.write("P")
+            elif r == 150 and g == 150 and b == 150:
+                f.write("E")
+
+        if i != 49: f.write("\n")
+
+if __name__ == "__main__":
+    if "--help" in argv or len(argv) != 3:
+        print("Tool for generating levels\n\nUsage: \n>level_generator.py {level_image_name} {level_file_name}\n\nExample: \n>level_generator.py level.png level")
+    else:
+        main()
